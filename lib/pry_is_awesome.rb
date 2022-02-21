74a8569761d43350cd9e6121c103cf224d55f57c
Layna Oyervides <laynaoyervides@gmail.com>
2022-02-21 15:40:51 -0700

    Completed Assignment

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -1,5 +1,8 @@
-require 'pry'
+#method that loads the pry library in the file when the app runs
+#works like import in JS
 
+require 'pry'
+#define method
 def prying_into_the_method
 	inside_the_method = "We're inside the method"
 	puts inside_the_method
@@ -8,5 +11,5 @@ def prying_into_the_method
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
 	puts this_variable_hasnt_been_interpreted_yet
 end
-
+#call method
 prying_into_the_method
